ca33d844f316a42c6bd1bf0e6ac587a9a14d6a3a
hima890 <hfibrahim90@gmail.com>
2024-09-08 13:44:44 +0200

    feature+update/update-user-model-delete-typeField+add-provider-model

--- APIs/v1/app/models/provider.py
+++ APIs/v1/app/models/provider.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" User Table Schema """
+""" Provider Table Schema """
 from flask import url_for
 from .. import db
 from datetime import datetime
 
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Provider(db.Model):
+    __tablename__ = 'providers'
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(150), nullable=False)
@@ -16,25 +16,24 @@ class User(db.Model):
     phone_number = db.Column(db.String(50), unique=True, nullable=True)
     profile_image = db.Column(db.String(200), nullable=True)
     gander = db.Column(db.String(50), nullable=True)
-    location = db.Column(db.String(150), nullable=True)
     password_hash = db.Column(db.String(128), nullable=False)
     is_active = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
-        """ Return a string representation of the User object. """
-        return "User email: {}, User name {}".format(self.email, self.first_name)
+        """ Return a string representation of the Provider object. """
+        return "Provider email: {}, Provider name {}".format(self.email, self.first_name)
 
     def to_dict(self):
-        """Convert the User object to a dictionary."""
-        # Check if the user has a profile image
+        """Convert the Provider object to a dictionary."""
+        # Check if the provider has a profile image
         if self.profile_image:
             profile_image_url = url_for('static', filename='profile_pics' + self.profile_image)
         else:
             # Set a default profile image
-            profile_image_url = 'defult_profile_image.png' 
-        # Return the user data as a dictionary
+            profile_image_url = 'defult_profile_image.png'
+        # Return the provider data as a dictionary
         return {
             'id': self.id,
             'first_name': self.first_name,
@@ -42,9 +41,7 @@ class User(db.Model):
             'username': self.username,
             'email': self.email,
             'phone_number': self.phone_number,
-            'type': self.type,
             'gander': self.gander,
-            'location': self.location,
             'is_active': self.is_active,
             'profile_image_path': profile_image_url,
             'created_at': self.created_at.isoformat(),
