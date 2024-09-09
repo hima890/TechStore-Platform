c34dfe4c458a69d6c363bad0b357f1cb993e26f2
hima890 <hfibrahim90@gmail.com>
2024-09-09 22:25:45 +0200

    feature/complate-optcode-end-point

--- APIs/v1/app/apis/opt_code.py
+++ APIs/v1/app/apis/opt_code.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """ OPT API Endpoints """
 from flasgger import swag_from
+from .. import db
 from .. import limiter
 from flask import request, jsonify
 from ..models.user import User
 from ..models.provider import Provider
 from . import optCode
 from .utils import generateOtpCode
+from ..utils.sendEmail import send_email
+from from .swaggerFile.swagger_docs import signup_d
+
 
 
 @optCode.route('/opt', methods=['GET'])
 @limiter.limit("5 per minute")
+@swag_from(opt_Code)
 def sendNewOptCode():
     # Get the user account email
     data = request.get_json()
@@ -33,4 +38,34 @@ def sendNewOptCode():
                 "message": "User acoount need to be activated"
                 }), 401
 
-    # 
+    # generate OTP and creation time
+    optCode, creationTime = generateOtpCode()
+    # Save to the user table and update exicting fields
+    user.opt_code = optCode
+    user.opt_code_time = creationTime
+    # Commit the changes to the database
+    db.session.commit()
+
+    # Send email whit the opt code to the user
+    userFirstName = user.
+    send_email(
+            email,
+            """
+            Dear {},
+
+            We received a request to reset your password. Please use the One-Time Password (OTP) below to reset your password. This code is valid for the next 10 minutes.
+
+            Your OTP Code: {}
+
+            If you did not request a password reset, please ignore this email or contact our support team immediately.
+
+            Best regards,  
+            TechStore Support Team
+
+            Note: For security reasons, this code will expire after 10 minutes.
+
+            """.format(
+                first_name,
+                optCode
+            )
+            )
