c2274f837cbe2681a0ee0f8772429b1e360c4d14
hima890 <hfibrahim90@gmail.com>
2024-09-09 21:49:57 +0200

    hotFix/blueprint-opt-code-register

--- APIs/v1/app/__init__.py
+++ APIs/v1/app/__init__.py
@@ -63,13 +63,13 @@ def create_app():
         # Import tabels after db is initialized
         from .models import User
         # Import the end-point
-        from .apis import endPoints, signUp, activation, signIn, opt_code
+        from .apis import endPoints, signUp, activation, signIn, optCode
         # Register the end-pointe
         app.register_blueprint(endPoints)
         app.register_blueprint(signUp)
         app.register_blueprint(activation)
         app.register_blueprint(signIn)
-        app.register_blueprint(opt_code)
+        app.register_blueprint(optCode)
         # Create the database tabels
         db.create_all()
 
