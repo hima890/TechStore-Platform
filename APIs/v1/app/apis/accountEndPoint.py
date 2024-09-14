e8329eaa6d7b9785027600779266e35b3ddf7308
hima890 <hfibrahim90@gmail.com>
2024-09-14 10:49:54 +0200

    feature/setup-account-password-endpoint

--- APIs/v1/app/apis/accountEndPoint.py
+++ APIs/v1/app/apis/accountEndPoint.py
@@ -3,6 +3,7 @@
 from flask import request, jsonify
 from flasgger import swag_from # type: ignore
 from flask_jwt_extended import jwt_required, get_jwt_identity # type: ignore
+from werkzeug.security import check_password_hash
 from . import account
 from .swaggerFile import accountDoc, accountUpdateDoc
 from ..models import User, Provider
@@ -104,6 +105,77 @@ def updateAccount():
     return jsonify({
         "status": "success",
         "message": "User account updated successfully",
+        "data": {
+            "userId": user.id,
+            "first name": user.first_name,
+            "last name": user.last_name,
+            "username": user.username,
+            "email": user.email,
+            "phone number": user.phone_number,
+            "gander": user.gander,
+            "location": user.location,
+            "profile image": user.profile_image
+            }
+        }), 200
+
+
+@account.route('/account-update-password', methods=['PUT'])
+@jwt_required()
+@limiter.limit("5 per minute")
+@swag_from(accountUpdatePasswordDoc)
+def updatePassword():
+    """Update the user's password"""
+    currentUserEmail = get_jwt_identity()
+    if not currentUserEmail:
+        return jsonify({
+            "status": "error",
+            "message": "Bad request, no user token"
+            }), 400
+
+    # Get the user
+    user = User.query.filter_by(email=currentUserEmail).first()
+    if not user:
+        user = Provider.query.filter_by(email=currentUserEmail).first()
+        return jsonify({
+                "status": "error",
+                "message": "User not found"
+                }), 404
+
+    # Check the request data
+    data = request.get_json()
+    if not data:
+        return jsonify({
+            "status": "error",
+            "message": "Bad request, no data provided"
+            }), 400
+
+    # Update the user's password
+    oldPassword = data.get("oldPassword")
+    newPassword = data.get("newPassword")
+    # Check if the request has a profile image
+    if not oldPassword or not newPassword:
+        return jsonify({
+            "status": "error",
+            "message": "Bad request, oldPassword and newPassword are required"
+            }), 400
+    
+    # Check if the old password is correct
+    if not check_password_hash(user.password_hash, oldPassword):
+        return jsonify({
+            "status": "error",
+            "message": "Old password is incorrect"
+            }), 400
+
+    # Update the user's password
+    user.password_hash = newPassword
+
+    # Save the user updates
+    db.session.commit()
+
+    # Return success response
+    return jsonify({
+        "status": "success",
+        "message": "User password updated successfully",
         "data": {
             "userId": user.id,
             "first name": user.first_name,
