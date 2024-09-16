56fa7c1b3792b667fb369f3573e05e29b4efdc3f
hima890 <hfibrahim90@gmail.com>
2024-09-16 13:06:15 +0200

    feature/new-product-end-point

--- APIs/v1/app/apis/productEndPoint.py
+++ APIs/v1/app/apis/productEndPoint.py
@@ -17,4 +17,53 @@ from .. import limiter
 # @swag_from(accountDoc)
 def addProduct():
     """Add products to the provider store account"""
-    return jsonify({'message': 'Add product'}), 200
+    # Get the profivder's email from the token
+    currentUserEmail = get_jwt_identity()
+    if not currentUserEmail:
+        return jsonify({
+            "status": "error",
+            "message": "Bad request, no user token"
+            }), 400
+    # Get the provider
+    user = Provider.query.filter_by(email=currentUserEmail).first()
+    if not user:
+        return jsonify({
+                "status": "error",
+                "message": "User not found"
+                }), 404
+
+    # Check the request data
+    if not request.form:
+        return jsonify({
+            "status": "error",
+            "message": "Bad request, no data provided"
+            }), 400
+
+    # Get the store id 
+    storeId = request.form.get('store_id')
+    name = request.form.get('name')
+    brand = request.form.get('brand')
+    category = request.form.get('category')
+    description = request.form.get('description')
+    price = request.form.get('price')
+    deliveryStatus = request.form.get('status')
+    image_1 = request.form.get('image_1')
+    image_2 = request.form.get('image_2')
+    image_3 = request.form.get('image_3')
+    image_4 = request.form.get('image_4')
+
+    # Create new product
+    newProduct = Product(storeId=storeId, name=name, brand=brand,
+                         category=category, description=description,
+                         price=price, deliveryStatus=deliveryStatus,
+                         image_1=image_1, image_2=image_2, image_3=image_3,
+                         image_4=image_4
+                         )
+    # Save the user updates
+    db.session.commit()
+    # Return success response
+    return jsonify({
+            "status": "success",
+            "message": "Product added successfully",
+            "data" : newProduct.to_dict()
+            }), 200
