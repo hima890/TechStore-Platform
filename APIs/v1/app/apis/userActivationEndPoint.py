02a914a7aa154ce786b8a2ddd5bf2a2781a6432d
Ruba-Salih <rubasalih900@gmail.com>
2024-09-21 01:26:21 +0300

    clean

--- APIs/v1/app/apis/userActivationEndPoint.py
+++ APIs/v1/app/apis/userActivationEndPoint.py
@@ -10,44 +10,40 @@ from .. import db
 from ..models import User, Provider
 
 
-
-
 @activation.route('/activate/<token>', methods=['GET'])
 @limiter.limit("5 per minute")
 @swag_from(activiationDoc)
 def activate_account(token):
     """Get the token from the URL and activate the user account"""
-    # Get the token from the URL
-    decoded_token = decode_token(token)  # Decode the JWT token
-    current_user =  decoded_token.get('sub')  # 'sub' is typically the identity
-    # Check it the token blong to a user
+    decoded_token = decode_token(token)
+    current_user =  decoded_token.get('sub')
+
     if not current_user:
-        # Return an error message if the token is invalid or expired
+
         return jsonify(
             {"error": "Invalid or expired token"}
             ), 400
-    # Filter the User table by email and check if the user exists
     user = User.query.filter_by(email=current_user).first()
-    # Check if the user exists
+
     if not user:
         user = Provider.query.filter_by(email=current_user).first()
         if not user:
-            # Return an error message if the user does not exist
+
             return jsonify(
                 {"status": "error",},
                 {"message": "User dose not exist"}
                 ), 407
-    # Check if the account already activated
+
     if user.is_active == True:
         return jsonify(
         {"status": "error",},
         {"message": "Account already activated"}
         ), 200
-    # If the user exists, activate the account
+
     user.is_active = True
-    # Save the changes to the database
+
     db.session.commit()
-    # Return a success message with a 200 status code
+
     return jsonify(
         {"status": "success",},
         {"message": "Account activated successfully"}
