762eb84279a35c4973048ea17ddc677ce6354dc7
hima890 <hfibrahim90@gmail.com>
2024-09-27 10:42:32 +0200

    hotFix/to_dict

--- APIs/v1/app/models/user.py
+++ APIs/v1/app/models/user.py
@@ -30,8 +30,6 @@ class User(db.Model):
         """Convert the User object to a dictionary."""
         return {
             'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
             'username': self.username,
             'email': self.email,
             'phone_number': self.phone_number,
